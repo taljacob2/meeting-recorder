c3d2c0f79247547e65cad08f086471461a9b3e2d
Tal Jacob <taljacob2@gmail.com>
2023-08-08 21:56:54 +0300

    Working On Converting To PowerShell
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ zoom/test.ps1
@@ -0,0 +1,32 @@
+# Specify the Execution times
+$TriggerTimes = @(
+    # '4:12:00pm',
+    # '4:12:10pm',
+    # '4:12:20pm',
+    # '2:00:00pm',
+    '9:06:00pm'
+)
+
+# Sort in chronologic order
+#  assuming the times format are the same
+$TriggerTimes = $TriggerTimes | Sort-Object
+
+foreach ($t in $TriggerTimes)
+{
+    # Past time ?
+    if((Get-Date) -lt (Get-Date -Date $t))
+    {
+        # Sleeping
+        while ((Get-Date -Date $t) -gt (Get-Date))
+        {
+            # Sleep for the remaining time
+            (Get-Date -Date $t) - (Get-Date) | Start-Sleep
+            echo "here"
+        }
+
+        # Trigger event
+        #  insert your code here
+        "# TriggerTime: '$t' - Executing my code here!"
+
+    }else{"Belong to the past: '$t'"}
+}
